f153960d4b18489c22cda2bf01862b54134dd9d7
Samyeol Son <samyeol0930@gmail.com>
2024-01-04 16:26:38 +0900

    Feat: 선배님 스토리와 기기 거래창을 전환하는 기능 구현
    - Gsap을 이용하여 부드럽게 표현
    - 이후에 nav바의 전환에도 적용할 예정

--- src/pages/MainPage/main.js
+++ src/pages/MainPage/main.js
@@ -1,9 +1,22 @@
-import data from '/src/pages/MainPage/data/data.json';
-import { insertLast } from '/src/lib/';
+import data1 from '/src/pages/MainPage/data/data1.json';
+import data2 from '/src/pages/MainPage/data/data2.json';
+import {
+  getNodes,
+  insertLast,
+  clearContents,
+  addClass,
+  removeClass,
+} from '/src/lib/';
 import gsap from 'gsap';
 
+const seniorStory = document.querySelector('.Main-menu-story');
+const seniorStoryBoard = document.querySelector('.Main-story-board');
+
+const exchange = document.querySelector('.Main-menu-exchange');
+const exchangeBoard = document.querySelector('.Main-exchange');
+
 function onLoad() {
-  data.forEach((item) => {
+  data1.forEach((item) => {
     const template = /* html */ `
     <li class="story">
       <a href="/">
@@ -22,12 +35,103 @@ function onLoad() {
 
     insertLast('.Main-story-board', template);
   });
+  gsap.from('.story', {
+    opacity: 0,
+    y: 10,
+    stagger: 0.05,
+  });
 }
 
 onLoad();
 
-gsap.from('.story', {
-  opacity: 0,
-  y: 0,
-  stagger: 0.05,
-});
+function handleExchange() {
+  const node = getNodes('.Main-menu-list li');
+  const nodes = getNodes('.page-list');
+
+  nodes.forEach((item) => {
+    removeClass(item, 'isActive');
+  });
+
+  addClass(exchangeBoard, 'isActive');
+
+  node.forEach((item) => {
+    removeClass(item, 'isActive');
+  });
+  addClass(exchange, 'isActive');
+  clearContents(exchangeBoard);
+  data2.forEach((item) => {
+    const template = /* html */ `
+  <li class="product">
+            <a href="/">
+              <figure>
+                <img src=${item.img} alt="" />
+              </figure>
+              <figcaption>
+                <div class="product-title">${item.title}</div>
+                <div class="product-info-container">
+                  <span class="product-location">${item.location}</span>
+                  <span>•</span>
+                  <span>${item.time}</span>
+                </div>
+                <div class="product-state-container">
+                  <span class="product-state">${item.state}</span>
+                  <span>${item.price}</span>
+                </div>
+              </figcaption>
+            </a>
+          </li>
+`;
+
+    insertLast('.Main-exchange', template);
+  });
+  gsap.from('.product', {
+    opacity: 0,
+    y: 10,
+    stagger: 0.1,
+  });
+}
+
+function handleSeniorStory() {
+  const node = getNodes('.Main-menu-list li');
+  const nodes = getNodes('.page-list');
+
+  nodes.forEach((item) => {
+    removeClass(item, 'isActive');
+  });
+  addClass(seniorStoryBoard, 'isActive');
+
+  node.forEach((item) => {
+    removeClass(item, 'isActive');
+  });
+
+  addClass(seniorStory, 'isActive');
+  clearContents(seniorStoryBoard);
+
+  data1.forEach((item) => {
+    const template = /* html */ `<li class="story">
+    <a href="/">
+            <figure>
+              <img src=${item.img} alt="" />
+            </figure>
+            <figcaption>
+              <div class="story-title">
+                ${item.title}
+              </div>
+              <div>${item.category} | ${item.name}</div>
+            </figcaption>
+            </a>
+          </li>
+`;
+
+    insertLast('.Main-story-board', template);
+  });
+
+  gsap.from('.story', {
+    opacity: 0,
+    y: 10,
+    stagger: 0.05,
+  });
+}
+
+seniorStory.addEventListener('click', handleSeniorStory);
+exchange.addEventListener('click', handleExchange);
