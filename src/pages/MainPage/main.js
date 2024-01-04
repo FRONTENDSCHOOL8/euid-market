9de4bdf67bfd897a9b89690c438ffcd430a45676
Samyeol Son <samyeol0930@gmail.com>
2024-01-04 18:05:45 +0900

    Modify: 기존 main.js 코드를 함수 형태로 변경
    - dataLoad(): 데이터 베이스에서 내용을 불러오는 함수
    
    - classClear(): nav와 page의 모든 active 클래스를 제거 하는 함수
    
    - classAdd(): 선택된 nav와 page의 클래스에 isActive를 추가하는 함수
    
    - activatePage(): 메뉴를 클릭하면 페이지를 활성화 하는 함수
    
    - animation: gsap micro animation을 함수형태로 변경

--- src/pages/MainPage/main.js
+++ src/pages/MainPage/main.js
@@ -2,6 +2,7 @@ import data1 from '/src/pages/MainPage/data/data1.json';
 import data2 from '/src/pages/MainPage/data/data2.json';
 import {
   getNodes,
+  getNode,
   insertLast,
   clearContents,
   addClass,
@@ -9,129 +10,122 @@ import {
 } from '/src/lib/';
 import gsap from 'gsap';
 
-const seniorStory = document.querySelector('.Main-menu-story');
-const seniorStoryBoard = document.querySelector('.Main-story-board');
+const seniorStory = getNode('.Main-menu-story');
+const seniorStoryBoard = getNode('.Main-story-board');
 
-const exchange = document.querySelector('.Main-menu-exchange');
-const exchangeBoard = document.querySelector('.Main-exchange');
+const exchange = getNode('.Main-menu-exchange');
+const exchangeBoard = getNode('.Main-exchange');
 
 function onLoad() {
-  data1.forEach((item) => {
-    const template = /* html */ `
-    <li class="story">
-      <a href="/">
-              <figure>
-                <img src=${item.img} alt="" />
-              </figure>
-              <figcaption>
-                <div class="story-title">
-                  ${item.title}
-                </div>
-                <div>${item.category} | ${item.name}</div>
-              </figcaption>
+  dataLoad(data1);
+  animation('.story');
+}
+
+function dataLoad(data) {
+  if (data === data1) {
+    data.forEach((item) => {
+      const template = /* html */ `
+      <li class="story">
+        <a href="/">
+                <figure>
+                  <img src=${item.img} alt="" />
+                </figure>
+                <figcaption>
+                  <div class="story-title">
+                    ${item.title}
+                  </div>
+                  <div>${item.category} | ${item.name}</div>
+                </figcaption>
+                </a>
+              </li>
+    `;
+
+      insertLast('.Main-story-board', template);
+    });
+  } else if (data === data2) {
+    data.forEach((item) => {
+      const template = /* html */ `
+    <li class="product">
+              <a href="/">
+                <figure>
+                  <img src=${item.img} alt="" />
+                </figure>
+                <figcaption>
+                  <div class="product-title">${item.title}</div>
+                  <div class="product-info-container">
+                    <span class="product-location">${item.location}</span>
+                    <span>•</span>
+                    <span>${item.time}</span>
+                  </div>
+                  <div class="product-state-container">
+                    <span class="product-state">${item.state}</span>
+                    <span>${item.price}</span>
+                  </div>
+                </figcaption>
               </a>
             </li>
   `;
 
-    insertLast('.Main-story-board', template);
+      insertLast('.Main-exchange', template);
+    });
+  }
+}
+
+function classClear(node1, node2) {
+  node1.forEach((item) => {
+    removeClass(item, 'isActive');
   });
-  gsap.from('.story', {
+
+  node2.forEach((item) => {
+    removeClass(item, 'isActive');
+  });
+}
+
+function classAdd(node1, node2) {
+  addClass(node1, 'isActive');
+  addClass(node2, 'isActive');
+}
+
+function activatePage(menuName) {
+  const classList = menuName.split(' ');
+  const menuList = getNodes('.Main-menu-list li');
+  const pageList = getNodes('.page-list');
+
+  if (menuName === 'Main-menu-exchange' && !classList.includes('isActive')) {
+    classClear(menuList, pageList);
+    classAdd(exchange, exchangeBoard);
+  }
+
+  if (menuName === 'Main-menu-story' && !classList.includes('isActive')) {
+    classClear(menuList, pageList);
+    classAdd(seniorStory, seniorStoryBoard);
+  }
+}
+
+function animation(node) {
+  gsap.from(node, {
     opacity: 0,
     y: 10,
     stagger: 0.05,
   });
+}
+
+function handleExchange(e) {
+  const menuName = e.currentTarget.className;
+  activatePage(menuName);
+  clearContents(exchangeBoard);
+  dataLoad(data2);
+  animation('.product');
+}
+
+function handleSeniorStory(e) {
+  const menuName = e.currentTarget.className;
+  activatePage(menuName);
+  clearContents(seniorStoryBoard);
+  dataLoad(data1);
+  animation('.story');
 }
 
 onLoad();
-
-function handleExchange() {
-  const node = getNodes('.Main-menu-list li');
-  const nodes = getNodes('.page-list');
-
-  nodes.forEach((item) => {
-    removeClass(item, 'isActive');
-  });
-
-  addClass(exchangeBoard, 'isActive');
-
-  node.forEach((item) => {
-    removeClass(item, 'isActive');
-  });
-  addClass(exchange, 'isActive');
-  clearContents(exchangeBoard);
-  data2.forEach((item) => {
-    const template = /* html */ `
-  <li class="product">
-            <a href="/">
-              <figure>
-                <img src=${item.img} alt="" />
-              </figure>
-              <figcaption>
-                <div class="product-title">${item.title}</div>
-                <div class="product-info-container">
-                  <span class="product-location">${item.location}</span>
-                  <span>•</span>
-                  <span>${item.time}</span>
-                </div>
-                <div class="product-state-container">
-                  <span class="product-state">${item.state}</span>
-                  <span>${item.price}</span>
-                </div>
-              </figcaption>
-            </a>
-          </li>
-`;
-
-    insertLast('.Main-exchange', template);
-  });
-  gsap.from('.product', {
-    opacity: 0,
-    y: 10,
-    stagger: 0.1,
-  });
-}
-
-function handleSeniorStory() {
-  const node = getNodes('.Main-menu-list li');
-  const nodes = getNodes('.page-list');
-
-  nodes.forEach((item) => {
-    removeClass(item, 'isActive');
-  });
-  addClass(seniorStoryBoard, 'isActive');
-
-  node.forEach((item) => {
-    removeClass(item, 'isActive');
-  });
-
-  addClass(seniorStory, 'isActive');
-  clearContents(seniorStoryBoard);
-
-  data1.forEach((item) => {
-    const template = /* html */ `<li class="story">
-    <a href="/">
-            <figure>
-              <img src=${item.img} alt="" />
-            </figure>
-            <figcaption>
-              <div class="story-title">
-                ${item.title}
-              </div>
-              <div>${item.category} | ${item.name}</div>
-            </figcaption>
-            </a>
-          </li>
-`;
-
-    insertLast('.Main-story-board', template);
-  });
-
-  gsap.from('.story', {
-    opacity: 0,
-    y: 10,
-    stagger: 0.05,
-  });
-}
-
 seniorStory.addEventListener('click', handleSeniorStory);
 exchange.addEventListener('click', handleExchange);
