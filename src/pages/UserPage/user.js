ccce353caffb3975c0d06bc41e994fdae459cc98
jio <pennicillium@daum.net>
2024-01-05 09:50:00 +0900

    BugFix : 프로필 페이지 프로필 수정 버튼 수정 - 화면 크기에 따라 수정 버튼 깨져서 마크업구조, 스타일링 수정 resolves #33

--- src/pages/UserPage/user.js
+++ src/pages/UserPage/user.js
@@ -1,1 +1,15 @@
 import './user.css';
+// import data from '/src/pages/UserPage/temp_data/userData.json';
+
+// //  유저 정보 데려오기
+// function getUserData(data) {
+//   const template =/*html */`
+
+//   `
+//   data.forEach((item) => {
+//     console.log(typeof item);
+//     let { userIdx, userNumber, userTemperature } = item;
+//   });
+// }
+// // console.log(data);
+// getUserData(data);
