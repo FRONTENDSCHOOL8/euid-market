71d51fb78a8030cc00d0fb6ecfcb9eadff07ce69
pistapixie <hleehslee@gmail.com>
2024-01-06 20:11:15 +0900

    Feat: 카테고리 카드 랜더링 구현

--- src/pages/LoginPage/category/category.js
+++ src/pages/LoginPage/category/category.js
@@ -1,9 +1,29 @@
 // import PocketBase from 'pocketbase';
 import data from './temp_data/data.json';
 
-import { getNode } from '../../../lib/index.js';
+import { getNode, tiger, insertLast } from '/src/lib';
 import { createPost, addClass, removeClass } from './util/dom/index.js';
 
+async function renderCategory() {
+  const response = await tiger.get(
+    'http://127.0.0.1:8090/api/collections/category/records'
+  );
+  const userData = response.data.items;
+  userData.forEach((item) => {
+    const template = /*html*/ `
+    <div class="login--category-card">
+    <div class="login--category-name">
+      <p class="paragraph-s">${item.main_category}</p>
+      <p class="heading-s">${item.sub_category}</p>
+    </div>
+    <img src="/src/assets/icons/login/plus.svg" alt="관심분야에 추가" />
+  </div>
+    `;
+    insertLast('body', template);
+  });
+}
+
+renderCategory();
 /* -------------- debugging area --------------*/
 function extractData() {
   console.log(data);
