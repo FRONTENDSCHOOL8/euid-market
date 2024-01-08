da0ece836e0c51c233f013c9b0be5c48c4095eac
dldnlee <sungeun8877@gmail.com>
2024-01-08 15:13:43 +0900

    Feat: 같이해요 게시판 게시물 추가 링크 기능 구현

--- src/pages/BoardPage/children_pages/boardContent/boardContent.js
+++ src/pages/BoardPage/children_pages/boardContent/boardContent.js
@@ -1,7 +1,11 @@
 import { insertLast } from "../../../../lib";
 import { renderTopBar } from "/src/components/general/renderTopBar.js";
-import { renderTogetherPosts } from "../../util/dom/index.js";
+import { renderTogetherPosts, relocateLink } from "../../util/index.js";
 import { getNode, insertBefore } from "/src/lib/index.js";
+
+
+
+
 
 
 const boardContainer = getNode(".board--container");
@@ -10,5 +14,8 @@ insertBefore(boardContainer, renderTopBar("withTitle"));
 insertLast(postContainer, renderTogetherPosts(postContainer));
 
 
+const createPostButton = getNode(".board--together-create-post");
 
-
+createPostButton.addEventListener("click", () => {
+  relocateLink("../createPost/")
+});
