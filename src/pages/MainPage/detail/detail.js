eaa3a4a78764fe27c22ede696f39cf89018325ca
Samyeol Son <samyeol0930@gmail.com>
2024-01-08 21:34:47 +0900

    Feat: 상품 상세페이지로 넘어갈 때 자연스러운 애니메이션 추가

--- src/pages/MainPage/detail/detail.js
+++ src/pages/MainPage/detail/detail.js
@@ -6,8 +6,11 @@ import {
   insertFirst,
   getPbImageURL,
 } from '/src/lib/';
+import gsap from 'gsap';
 import pb from '/src/lib/utils/pocketbase.js';
 import { productPriceTemplate } from '/src/pages/MainPage/template.js';
+
+const tl = gsap.timeline();
 
 const hash = window.location.hash.slice(1);
 const productImg = getNode('.Main-visual > img');
@@ -18,3 +21,10 @@ const product = await pb.collection('product_list').getOne(hash);
 attr(productImg, 'src', getPbImageURL(product));
 insertLast(mainContent, productDetailTemplate(product));
 insertFirst(price, productPriceTemplate(product));
+
+tl.from('body', {
+  opacity: 0,
+  x: '100%',
+  ease: 'power2.inOut',
+  duration: 0.7,
+});
