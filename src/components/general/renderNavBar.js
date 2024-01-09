35fb3a6f8862830ba929c58f732f1351e7dd4f3a
dldnlee <sungeun8877@gmail.com>
2024-01-09 11:30:21 +0900

    Feat: 네비게이션바 눌렀을 때 페이지 이동 및 아이콘 변경

--- src/components/general/renderNavBar.js
+++ src/components/general/renderNavBar.js
@@ -11,32 +11,32 @@ export function renderNavBar() {
   <nav class="bottom-bar-container">
     <ul>
       <li>
-        <a href="/src/pages/MainPage/">
-          <img src="/src/assets/icons/general/home.svg" alt="홈">
+        <a href="/src/pages/MainPage/" data-index=1>
+          <img src=${local.getItem("curr_page") === "main" ? "/src/assets/icons/general/fullHome.svg" : "/src/assets/icons/general/home.svg"} alt="홈">
           <p class="paragraph-s">홈</p>
         </a>
       </li>
       <li>
-        <a href="/src/pages/BoardPage/">
-          <img src="/src/assets/icons/general/fullFile.svg" alt="게시판">
+        <a href="/src/pages/BoardPage/" data-index=2>
+          <img src=${local.getItem("curr_page") === "board" ? "/src/assets/icons/general/fullFile.svg" : "/src/assets/icons/general/file.svg"} alt="게시판">
           <p class="paragraph-s">게시판</p>
         </a>
       </li>
       <li>
-        <a href="/src/pages/MainPage/">
-          <img src="/src/assets/icons/general/map.svg" alt="내 근처">
+        <a href="/src/pages/MainPage/" data-index=3>
+          <img src=${local.getItem("curr_page") === "location" ? "/src/assets/icons/general/fullMap.svg" : "/src/assets/icons/general/map.svg"} alt="내 근처">
           <p class="paragraph-s">내 근처</p>
         </a>
       </li>
       <li>
-        <a href="/src/pages/BoardPage/children_pages/boardContent/">
-          <img src="/src/assets/icons/general/chat.svg" alt="채팅">
+        <a href="/src/pages/BoardPage/children_pages/boardContent/" data-index=4>
+          <img src=${local.getItem("curr_page") === "chat" ? "/src/assets/icons/general/fullChat.svg" : "/src/assets/icons/general/chat.svg"} alt="채팅">
           <p class="paragraph-s">채팅</p>
         </a>
       </li>
       <li>
-        <a href="/src/pages/UserPage/">
-          <img src="/src/assets/icons/general/my.svg" alt="나의 이듬">
+        <a href="/src/pages/UserPage/" data-index=5>
+          <img src=${local.getItem("curr_page") === "user" ? "/src/assets/icons/general/fullMy.svg" : "/src/assets/icons/general/my.svg"} alt="나의 이듬">
           <p class="paragraph-s">나의 이듬</p>
         </a>
       </li>
@@ -47,11 +47,24 @@ export function renderNavBar() {
   insertLast("body", template);
 
   function handleNavBar(e) {
+    
     const target = e.target.closest("nav li a");
+    console.log(target);
     if(!target) return;
+
+    const pages = {
+      "1": () => local.setItem("curr_page", "main"),
+      "2": () => local.setItem("curr_page", "board"),
+      "3": () => local.setItem("curr_page", "location"),
+      "4": () => local.setItem("curr_page", "chat"),
+      "5": () => local.setItem("curr_page", "profile")
+    };
+    
+    const changeCurrPage = pages[target.dataset.index];
+    changeCurrPage();
+  }
   
 
-  }
   
   const navBarContainer = getNode(".bottom-bar-container");
   navBarContainer.addEventListener("click", handleNavBar);
