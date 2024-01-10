56473bd3033e96889d3e53650d5885a76b348c91
jio <pennicillium@daum.net>
2024-01-10 20:13:35 +0900

    Feat: 연령, 나이 공개여부 버튼 기능 구현 resolve #58

--- src/pages/UserPage/children_pages/profileModify/profileModify.js
+++ src/pages/UserPage/children_pages/profileModify/profileModify.js
@@ -3,39 +3,39 @@ import pb from '/src/lib/api/pocketbase';
 import { profilePublicButtonTemplate } from '/src/pages/UserPage/template';
 
 import { getNode, insertAfter } from '/src/lib/';
-
-
-
-
+const TEST_USER_ID = 'bexmuprbriobf8v';
 (async () => {
   //배포전 수정 !!!!!!!!!!!!!!!1
-  const TEST_USER_ID = 'c24hzewie4gweri';
-  
-  // 기본 렌더링 !
+  const profileKeyword = getNode('.profile--modify-profile-keyword');
+  // 버튼 기본 렌더링 !
   const userPrivacyResult = (
     await pb
       .collection('user_privacy')
       .getList(1, 10, { filter: `user_id = "${TEST_USER_ID}" ` })
   ).items[0];
-  
-  const profileKeyword = getNode('.profile--modify-profile-keyword');
-  
   insertAfter(profileKeyword, profilePublicButtonTemplate(userPrivacyResult));
-  
-  const tempData = {};
-  
-  // 버튼이벤트
+})().then(() => {
+  let tempData = { gender_is_public: '' };
+
+  // 버튼이벤트 바인딩 !
   const genderIsPublic = getNode('.profile--gender-is-public');
-  
-  function handleGenderToggle(e) {
-    tempData['gender_is_public'] = Boolean(e.target.dataset.is_public);
-    Array.from(genderIsPublic.children).forEach((item) => {
-      item.dataset.is_public = 'false';
-      item.classList.remove('is-active');
-    });
+  const ageIsPublic = getNode('.profile--age-is-public');
+
+  function handlePublicToggle(e) {
+    const type = e.currentTarget.dataset.type;
+    tempData[`${type}_is_public`] = e.target.dataset.is_public;
+    if (type === 'gender') {
+      Array.from(genderIsPublic.children).forEach((item) => {
+        item.classList.remove('is-active');
+      });
+    } else if (type === 'age') {
+      Array.from(ageIsPublic.children).forEach((item) => {
+        item.classList.remove('is-active');
+      });
+    }
     e.target.classList.add('is-active');
   }
-  
-  genderIsPublic.addEventListener('click', handleGenderToggle);
 
-})();
+  genderIsPublic.addEventListener('click', handlePublicToggle);
+  ageIsPublic.addEventListener('click', handlePublicToggle);
+});
