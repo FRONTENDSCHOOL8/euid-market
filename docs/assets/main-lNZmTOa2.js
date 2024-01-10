c9fa6ee933e3cf1ffb1da3a0f7547a721e66d828
pistapixie <hleehslee@gmail.com>
2024-01-10 15:52:30 +0900

    Feat: 로그인 페이지 폰 번호 입력창 구현 - 입력창에 폰번호 형식 제한 설정 - 버튼 활성화 구현

---
+++ docs/assets/main-lNZmTOa2.js
@@ -0,0 +1,1 @@
+import{r}from"./renderNavBar-gTwwGWGl.js";r();
--- src/pages/LoginPage/login/login.js
+++ src/pages/LoginPage/login/login.js
@@ -1,1 +1,56 @@
-import { formatPhoneNumber } from '/src/pages/LoginPage/util/formatPhoneNumber.js'; // 경로는 실제 utils.js 파일 위치에 맞게 조정해야 합니다.
+import { generateRandomCode, phoneNumFormat } from '/src/pages/LoginPage/util/'; 
+import {getNode, getNodes, tiger} from '/src/lib/'
+
+
+function requestVerification() {
+  // 활성화된 버튼 확인
+  const verifyButton = document.getElementById('verify-button');
+  if (!verifyButton.hasAttribute('aria-disabled')) {
+    // .login--code-section, #start-button, .login--nocode 요소 표시
+    const codeSection = document.querySelector('.login--code-section');
+    const startButton = document.getElementById('start-button');
+    const noCodeLink = document.querySelector('.login--nocode');
+    codeSection.classList.remove('hidden');
+    startButton.classList.remove('hidden');
+    noCodeLink.classList.remove('hidden');
+
+    // .login--signup-email 요소 숨김
+    const signupEmailLink = document.querySelector('.login--signup-email');
+    signupEmailLink.classList.add('hidden');
+  }
+}
+
+
+function enableVerificationButton() {
+  const phoneInput = document.getElementById('phone');
+  const verifyButton = document.getElementById('verify-button');
+
+  phoneInput.addEventListener('keyup', () => {
+    phoneNumFormat(phoneInput);
+
+    // 휴대폰 번호 형식이 올바르면 버튼 활성화
+    if (phoneInput.value.match(/^\d{3} \d{4} \d{4}$/)) {
+      verifyButton.removeAttribute('disabled');
+      verifyButton.classList.add('active');
+    } else {
+      verifyButton.setAttribute('disabled', 'true');
+      verifyButton.classList.remove('active');
+    }
+  });
+}
+
+// DOMContentLoaded 이벤트 리스너
+document.addEventListener('DOMContentLoaded', () => {
+  enableVerificationButton();
+
+  const verifyButton = document.getElementById('verify-button');
+  if (verifyButton) {
+    verifyButton.addEventListener('click', requestVerification);
+  }
+});
+
+
+
+
+
+
