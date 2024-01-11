a81d58d1046b4b3543b020cd20b80ed7be5279e0
dldnlee <sungeun8877@gmail.com>
2024-01-12 01:46:45 +0900

    Feat: 같이해요 게시물 정보 프로필 사진 및 유저네임 렌더링 구현

--- src/pages/BoardPage/util/dom/renderPostInfo.js
+++ src/pages/BoardPage/util/dom/renderPostInfo.js
@@ -6,10 +6,10 @@ import { getUserData, getUserProfilePicture } from "../index";
 export async function renderPostInfo(container, id) {
   
   const data = await pb.collection('posts').getOne(id);
-  let userID;
+  let user;
   getUserData(data.created_by).then((creator) => {
-    userID = creator;
-    getUserProfilePicture(userID).then((imageURL) => {
+    user = creator;
+    getUserProfilePicture(user).then((imageURL) => {
       const template = /* html */ 
       `
       <div class="board--post-info">
@@ -21,11 +21,11 @@ export async function renderPostInfo(container, id) {
         <div role="header">
           <span class="label-l" style=${data.status === "모집중" ? "color:#5A85EE;" : "color:#919191;"}>${data.status}</span>
           <h1 class="label-l">${data.title}</h1>
-          <figure>
+          <figure class="board--post-info-requirements">
             <img src="/src/assets/icons/board/people.svg" alt="" />
             <figcaption class="paragraph-m">${data.requirements}</figcaption>
           </figure>
-          <figure>
+          <figure class="board--post-info-requirements">
             <img src="/src/assets/icons/board/fullCalendar.svg" alt="" />
             <figcaption class="paragraph-m">스터디</figcaption>
           </figure>
@@ -35,12 +35,63 @@ export async function renderPostInfo(container, id) {
     
         <h3 class="label-m">참여중인 이웃 ${data.curr_people}/${data.max_people}</h3>
     
-        <figure>
-          <img src=${imageURL} alt="" />
+        <figure class="board--post-info-creator">
+          <img src=${imageURL} alt="유저 사진" />
           
-          <span>멋사드미</span>
-          <span>모임장</span>
-          <h3>연남동 인증 4회</h3>
+          <div>
+            <div>
+              <span class="board--post-info-creator-username label-m">${user.user_nickname}</span>
+              <span class="board--post-info-creator-role">모임장</span>
+            </div>
+            <p class="board--post-info-creator-info">연남동 인증 4회</p>
+          </div>
+          
+        </figure>
+    
+        <button class="board--fixed-button">
+          참여하기
+        </button>
+      
+      </div>
+      `
+      insertLast(container, template);
+    })
+    .catch(() => {
+      const template = /* html */ 
+      `
+      <div class="board--post-info">
+        <figure class="board--post-info-badge">
+          <span>🧀</span>
+          <figcaption>${data.type}</figcaption>
+        </figure>
+    
+        <div role="header">
+          <span class="label-l" style=${data.status === "모집중" ? "color:#5A85EE;" : "color:#919191;"}>${data.status}</span>
+          <h1 class="label-l">${data.title}</h1>
+          <figure class="board--post-info-requirements">
+            <img src="/src/assets/icons/board/people.svg" alt="" />
+            <figcaption class="paragraph-m">${data.requirements}</figcaption>
+          </figure>
+          <figure class="board--post-info-requirements">
+            <img src="/src/assets/icons/board/fullCalendar.svg" alt="" />
+            <figcaption class="paragraph-m">스터디</figcaption>
+          </figure>
+        </div>
+    
+        <p class="paragraph-m">${data.content}</p>
+    
+        <h3 class="label-m">참여중인 이웃 ${data.curr_people}/${data.max_people}</h3>
+    
+        <figure class="board--post-info-creator">
+          <div class="board--post-info-no-pic"></div>
+          
+          <div>
+            <div>
+              <span class="board--post-info-creator-username label-m">${user.user_nickname}</span>
+              <span class="board--post-info-creator-role">모임장</span>
+            </div>
+            <p class="board--post-info-creator-info">연남동 인증 4회</p>
+          </div>
           
         </figure>
     
