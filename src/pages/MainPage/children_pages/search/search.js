868d6de27e1f36d1d0413379ef2ce0cece7809de
Samyeol Son <samyeol0930@gmail.com>
2024-01-12 23:03:50 +0900

    Modify: search.js의 파일 구조 통일화

--- src/pages/MainPage/children_pages/search/search.js
+++ src/pages/MainPage/children_pages/search/search.js
@@ -1,10 +1,29 @@
 import { getNode, insertLast, clearContents } from '/src/lib/';
-import pb from '/src/lib/api/pocketbase.js';
+
 import { exchangeTemplate } from '/src/pages/MainPage/util/';
+
+import pb from '/src/lib/api/pocketbase.js';
 
 const inputBox = getNode('.search-container > input');
 const searchList = getNode('.search-list');
 const back = getNode('.search-container > img');
+
+async function inputHandler(e) {
+  clearContents(searchList);
+
+  const search = await pb.collection('product_list').getFullList({
+    filter: `title ~ "${e.target.value}"`,
+  });
+
+  if (search.length === 0) {
+    clearContents(searchList);
+    console.log('없음');
+    return;
+  }
+  search.forEach((item) => {
+    insertLast(searchList, exchangeTemplate(item));
+  });
+}
 
 function debounce(func, delay = 100) {
   let timer = null;
@@ -17,25 +36,6 @@ function debounce(func, delay = 100) {
       func.apply(this, args);
     }, delay);
   };
-}
-
-async function inputHandler(e) {
-  clearContents(searchList);
-  if (e.target.value === '') {
-    clearContents(searchList);
-  } else {
-    const search = await pb.collection('product_list').getFullList({
-      filter: `title ~ "${e.target.value}"`,
-    });
-    if (search.length === 0) {
-      clearContents(searchList);
-      console.log('없음');
-      return;
-    }
-    search.forEach((item) => {
-      insertLast(searchList, exchangeTemplate(item));
-    });
-  }
 }
 
 inputBox.addEventListener('input', debounce(inputHandler));
